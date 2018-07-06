4f77913a4c742ab10d79abcdd0873402978e97dd
OvchinnikovDmitrii <ovchinnikov.dmitrii@gmail.com>
2018-07-06 17:48:30 +0300

    Script fix.

---
+++ tests/transcode_h264.bat
@@ -0,0 +1,6 @@
+set curDir=%cd%
+
+..\_build-mingw_gcc_x64-debug\HandBrakeCLI.exe -i %cd%\%1 -e x264 -o %cd%\%~n1_h264.mkv
+.\compare.bat %1 %~n1_x264.mkv
+
+
--- tests/transcode_x264.bat
+++
@@ -1,6 +0,0 @@
-set curDir=%cd%
-
-..\_build-mingw_gcc_x64-debug\HandBrakeCLI.exe -i %cd%\%1 -e x264 -o %cd%\%~n1_x264.mkv
-.\compare.bat %1 %~n1_x264.mkv
-
-
